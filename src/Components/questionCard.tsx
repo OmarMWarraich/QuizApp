07e526c2a1a90dfa2a043ca0bf03b83bdc9c2e51
OmarMWarraich <earthling986@gmail.com>
2020-10-24 02:04:09 +0500

    Final Commit

--- src/Components/questionCard.tsx
+++ src/Components/questionCard.tsx
@@ -3,7 +3,7 @@ import { questionPropsType } from "./../Types/types";
 import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
-import { FormControl } from '@material-ui/core/FormControl';
+import FormControl from '@material-ui/core/FormControl';
 
 const QuestionCard: React.FC<questionPropsType> = ({
   question,
